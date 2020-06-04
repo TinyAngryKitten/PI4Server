5980aa37cf724a0ac753acefa271ddfd3ebff47d
sander høyvik <sander.hoyvik@gmail.com>
2020-06-04 21:39:06 +0200

    influx and telegraf

---
+++ runMqtt.sh
@@ -0,0 +1,1 @@
+docker run -it -p 1883:1883 -p 9001:9001 -v /home/pi/PI4Server/mosquitto/mosquitto.conf:/mosquitto/config/mosquitto.conf --name mosquitto eclipse-mosquitto
---
+++ runTelegraf.sh
@@ -0,0 +1,1 @@
+docker run  -v /home/pi/PI4Server/telegraf/:/etc/telegraf:ro --name telegraf telegraf
