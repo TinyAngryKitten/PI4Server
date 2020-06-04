eb1f83fe60248633e08438cb9b1a579242a9190d
sander høyvik <sander.hoyvik@gmail.com>
2020-06-04 21:42:18 +0200

    newlines in runmqtt

--- runMqtt.sh
+++ runMqtt.sh
@@ -1,1 +1,5 @@
-docker run -it -p 1883:1883 -p 9001:9001 -v /home/pi/PI4Server/mosquitto/mosquitto.conf:/mosquitto/config/mosquitto.conf --name mosquitto eclipse-mosquitto
+docker run -it \
+ -p 1883:1883 -p 9001:9001 \
+ -v /home/pi/PI4Server/mosquitto/mosquitto.conf:/mosquitto/config/mosquitto.conf \
+ --name mosquitto \
+ eclipse-mosquitto
