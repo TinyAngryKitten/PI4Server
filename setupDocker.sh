720472fd36761db91e2d82aa3cb9b0fbae95f55f
sander høyvik <sander.hoyvik@gmail.com>
2020-06-08 23:47:56 +0200

    docker install + mqtt stack

---
+++ setupDocker.sh
@@ -0,0 +1,10 @@
+sudo apt install -y docker.io
+#maybe install docker too? idk the difference...
+sudo apt install -y git
+
+#ensure user has right to access docker
+usermod ubuntu -aG docker
+
+3sudo apt-get install -y libffi-dev libssl-dev
+
+sudo apt-get install -y python3 python3-pip
