a92d2086eff75ce723aff011f618b5e5c1d3a095
sander høyvik <sander.hoyvik@gmail.com>
2020-06-09 00:42:08 +0200

    cleanup of redundant/old files

--- runInflux.sh
+++
@@ -1,6 +0,0 @@
-sudo docker run -d -p 8086:8086 \
-      -e INFLUXDB_DB=sensordata \
-      -e INFLUXDB_USER=telegraf -e INFLUXDB_USER_PASSWORD=telegraf \
-      -v /home/pi/PI4Server/influxdb/db:/var/lib/influxdb \
-      -v /home/pi/PI4Server/influxdb/influxdb.conf:/etc/influxdb/influxdb.conf:ro \
-      --name influxdb influxdb
--- runTelegraf.sh
+++
@@ -1,1 +0,0 @@
-docker run -d -v /home/pi/PI4Server/telegraf/:/etc/telegraf:ro --name telegraf telegraf
