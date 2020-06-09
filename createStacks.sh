3114342a0ad3dc859a249cc8479e50711e480775
sander høyvik <sander.hoyvik@gmail.com>
2020-06-09 16:55:38 +0200

    added faas to deployment, remove swarmpit
    Swarmpit wont deploy, temporarily disable it

--- createStacks.sh
+++ createStacks.sh
@@ -1,2 +1,3 @@
-docker stack deploy -c swarmpit/docker-compose.yml swarmpit
+#docker stack deploy -c swarmpit/docker-compose.yml swarmpit
 docker stack deploy -c mqtt-stack.yml mqtt
+sh faas/deploy_stack.sh --no-auth
