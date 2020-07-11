2d13ab793916fbc07f1c5fe33c80912b6fa4bbde
sander høyvik <sander.hoyvik@gmail.com>
2020-07-11 17:20:29 +0200

    add pihole

--- createStacks.sh
+++ createStacks.sh
@@ -1,2 +1,10 @@
+mkdir mqtt_microservices/mongodb
+mkdir swarmpit/db-data
+mkdir swarmpit/influxdb-data
+mkdir pihole/etc-pihole
+mkdir pihole/etc-dnsmasq.d
+
+docker network create --driver=overlay proxy
+
 docker stack deploy -c swarmpit/docker-compose.yml swarmpit
 docker stack deploy -c mqtt_microservices/mqtt-stack.yml mqtt_microservices
