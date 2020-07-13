0cd79dded09b622a44339bafa6dd323ceb7b5153
sander høyvik <sander.hoyvik@gmail.com>
2020-07-13 13:24:39 +0200

    added chronograf

--- createStacks.sh
+++ createStacks.sh
@@ -3,8 +3,25 @@ mkdir swarmpit/db-data
 mkdir swarmpit/influxdb-data
 mkdir pihole/etc-pihole
 mkdir pihole/etc-dnsmasq.d
+mkdir mqtt_microservices/chronograf
 
-docker network create --driver=overlay proxy
+docker volume create --driver local \
+      --opt type=nfs \
+      --opt o=nfsvers=4,addr=10.0.0.96,rw \
+      --opt device=:/home/ubuntu/PI4Server/pihole/etc-pihole \
+      piholevolume
+
+docker volume create --driver local \
+      --opt type=nfs \
+      --opt o=nfsvers=4,addr=10.0.0.96,rw \
+      --opt device=:/home/ubuntu/PI4Server/pihole/etc-dnsmasq.d \
+      dnsvolume
+
+docker volume create --driver local \
+      --opt type=nfs \
+      --opt o=nfsvers=4,addr=10.0.0.96,rw \
+      --opt device=:/home/ubuntu/PI4Server/mqtt_microservices/chronograf \
+      chronografvolume
 
 docker stack deploy -c swarmpit/docker-compose.yml swarmpit
 docker stack deploy -c mqtt_microservices/mqtt-stack.yml mqtt_microservices
