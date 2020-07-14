a6ef50c0f9e837d997bf9dbc2392a2ca59e5a9c9
sander høyvik <sander.hoyvik@gmail.com>
2020-07-14 18:19:54 +0200

    common volumes

--- createStacks.sh
+++ createStacks.sh
@@ -23,6 +23,49 @@ docker volume create --driver local \
       --opt device=:/home/ubuntu/PI4Server/mqtt_microservices/chronograf \
       chronografvolume
 
+docker volume create --driver local \
+      --opt type=nfs \
+      --opt o=nfsvers=4,addr=10.0.0.96,rw \
+      --opt device=:/home/ubuntu/PI4Server/swarmpit/db-data \
+      dbdata
+
+docker volume create --driver local \
+      --opt type=nfs \
+      --opt o=nfsvers=4,addr=10.0.0.96,rw \
+      --opt device=:/home/ubuntu/PI4Server/swarmpit/influxdb-data \
+      influxdata
+
+docker volume create --driver local \
+      --opt type=nfs \
+      --opt o=nfsvers=4,addr=10.0.0.96,rw \
+      --opt device=:/home/ubuntu/PI4Server/mqtt_microservices/mosquitto/mosquitto.conf \
+      mqttconf
+
+docker volume create --driver local \
+      --opt type=nfs \
+      --opt o=nfsvers=4,addr=10.0.0.96,rw \
+      --opt device=:/home/ubuntu/PI4Server/mqtt_microservices/mongodb \
+      mongodbdata
+
+docker volume create --driver local \
+      --opt type=nfs \
+      --opt o=nfsvers=4,addr=10.0.0.96,rw \
+      --opt device=:/home/ubuntu/PI4Server/mqtt_microservices/telegraf \
+      telegrafdata
+
+docker volume create --driver local \
+      --opt type=nfs \
+      --opt o=nfsvers=4,addr=10.0.0.96,rw \
+      --opt device=:/home/ubuntu/PI4Server/mqtt_microservices/influxdb/db \
+      mqttinfluxdb
+
+docker volume create --driver local \
+      --opt type=nfs \
+      --opt o=nfsvers=4,addr=10.0.0.96,rw \
+      --opt device=:/home/ubuntu/PI4Server/mqtt_microservices/influxdb/influxdb.conf \
+      mqttinfluxconf
+
+
 docker stack deploy -c swarmpit/docker-compose.yml swarmpit
 docker stack deploy -c mqtt_microservices/mqtt-stack.yml mqtt_microservices
 docker stack deploy -c traefik/traefik-stack.yml traefik
