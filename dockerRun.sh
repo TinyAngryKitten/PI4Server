0784a40e2ccea1bb677e30c05b004a068afd9ecb
sander høyvik <sander.hoyvik@gmail.com>
2020-07-15 10:27:57 +0200

    Create dockerRun.sh

---
+++ dockerRun.sh
@@ -0,0 +1,27 @@
+docker run -d \
+  -v chronografvolume:/var/lib/chronograf \
+  -p 8888:8888 \
+  -l traefik.enable=true \
+  -l traefik.frontend.rule=PathPrefixStrip:/chronograf \
+  -l traefik.port=8888 \
+  -l traefik.backend=mqtt.chronograf \
+  -l traefik.http.services.chronograf.loadbalancer.server.port=8888
+  chronograf:1.8
+
+docker run -d \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  #-v $PWD/traefik.toml:/traefik.toml \
+  -p 80:80 \
+  -p 443:443 \
+  -l traefik.port=8080 \
+  -l api=true \
+  -l api.dashboard=true \
+  -l docker=true \
+  -l docker.exposedbydefault=true \
+  -l docker.network=proxy \
+  -l consulcatalog=true \
+  -l consulcatalog.endpoint=10.0.0.96:8500 \
+  -l consulcatalog.exposedByDefault=true \
+  --network proxy \
+  --name traefik \
+  traefik:1.7.2
