52358ca8b6472ef91c527c6d50d48e35ac31baba
sander høyvik <sander.hoyvik@gmail.com>
2020-11-08 17:50:42 +0100

    added docker machine

--- createStacks.sh
+++ createStacks.sh
@@ -1,85 +1,95 @@
-echo "creating docker volume folders..."
-mkdir mqtt_microservices/mongodb
-mkdir swarmpit/db-data
-mkdir swarmpit/influxdb-data
-mkdir pihole/etc-pihole
-mkdir pihole/etc-dnsmasq.d
-mkdir mqtt_microservices/chronograf
-mkdir homebridge/data
+export rootFolder=/mnt/raidstorage/rpidata #/home/ubuntu/PI4Server
+export keyPath=/Users/sanderhoyvik/Keys/sshkey
 
+echo "creating docker volume folders..."
+
+ssh -p 22 -i $keyPath ubuntu@10.0.0.96 << EOF
+mkdir $rootFolder/mqtt_microservices/mongodb
+mkdir $rootFolder/swarmpit/db-data
+mkdir $rootFolder/swarmpit/influxdb-data
+mkdir $rootFolder/pihole/etc-pihole
+mkdir $rootFolder/pihole/etc-dnsmasq.d
+mkdir $rootFolder/mqtt_microservices/chronograf
+mkdir $rootFolder/homebridge/data
+EOF
+
+echo "Connecting to remote docker host"
+eval $(docker-machine env pi4Cluster)
+
+echo "Create docker resources"
 #Create a network that dont overlap with the local network
-#docker network create \
-#    --driver overlay \
-#    --ingress \
-#    --subnet 172.16.0.0/16 \
-#    --gateway 172.16.0.1 \
-#    ingress
+docker network create \
+    --driver overlay \
+    --ingress \
+    --subnet 172.16.0.0/16 \
+    --gateway 172.16.0.1 \
+    ingress
 
 echo "Creating docker volumes..."
 docker volume create --driver local \
       --opt type=nfs \
       --opt o=nfsvers=4,addr=10.0.0.96 \
-      --opt device=:/home/ubuntu/PI4Server/pihole/etc-pihole \
+      --opt device=:$rootFolder/pihole/etc-pihole \
       piholevolume
 
 docker volume create --driver local \
       --opt type=nfs \
       --opt o=nfsvers=4,addr=10.0.0.96 \
-      --opt device=:/home/ubuntu/PI4Server/traefik \
+      --opt device=:$rootFolder/traefik \
       traefikvolume
 
 docker volume create --driver local \
       --opt type=nfs \
       --opt o=nfsvers=4,addr=10.0.0.96 \
-      --opt device=:/home/ubuntu/PI4Server/pihole/etc-dnsmasq.d \
+      --opt device=:$rootFolder/pihole/etc-dnsmasq.d \
       dnsvolume
 
 docker volume create --driver local \
       --opt type=nfs \
       --opt o=nfsvers=4,addr=10.0.0.96 \
-      --opt device=:/home/ubuntu/PI4Server/mqtt_microservices/chronograf \
+      --opt device=:$rootFolder/mqtt_microservices/chronograf \
       chronografvolume
 
 docker volume create --driver local \
       --opt type=nfs \
       --opt o=nfsvers=4,addr=10.0.0.96 \
-      --opt device=:/home/ubuntu/PI4Server/swarmpit/db-data \
+      --opt device=:$rootFolder/swarmpit/db-data \
       dbdata
 
 docker volume create --driver local \
       --opt type=nfs \
       --opt o=nfsvers=4,addr=10.0.0.96 \
-      --opt device=:/home/ubuntu/PI4Server/swarmpit/influxdb-data \
+      --opt device=:$rootFolder/swarmpit/influxdb-data \
       influxdata
 
 docker volume create --driver local \
       --opt type=nfs \
       --opt o=nfsvers=4,addr=10.0.0.96 \
-      --opt device=:/home/ubuntu/PI4Server/mqtt_microservices/mosquitto/mosquitto.conf \
+      --opt device=:$rootFolder/mqtt_microservices/mosquitto/mosquitto.conf \
       mqttconf
 
 docker volume create --driver local \
       --opt type=nfs \
       --opt o=nfsvers=4,addr=10.0.0.96 \
-      --opt device=:/home/ubuntu/PI4Server/mqtt_microservices/mongodb \
+      --opt device=:$rootFolder/mqtt_microservices/mongodb \
       mongodbdata
 
 docker volume create --driver local \
       --opt type=nfs \
       --opt o=nfsvers=4,addr=10.0.0.96 \
-      --opt device=:/home/ubuntu/PI4Server/mqtt_microservices/telegraf \
+      --opt device=:$rootFolder/mqtt_microservices/telegraf \
       telegrafdata
 
 docker volume create --driver local \
       --opt type=nfs \
       --opt o=nfsvers=4,addr=10.0.0.96 \
-      --opt device=:/home/ubuntu/PI4Server/mqtt_microservices/influxdb/db \
+      --opt device=:$rootFolder/mqtt_microservices/influxdb/db \
       mqttinfluxdb
 
 docker volume create --driver local \
       --opt type=nfs \
       --opt o=nfsvers=4,addr=10.0.0.96 \
-      --opt device=:/home/ubuntu/PI4Server/mqtt_microservices/influxdb \
+      --opt device=:$rootFolder/mqtt_microservices/influxdb \
       mqttinfluxconf
 
 docker pull tinyangrykitten/hueserver:latest
@@ -94,5 +104,7 @@ echo "Deploying mqtt microservices stack"
 docker stack deploy -c mqtt_microservices/mqtt-stack.yml mqtt_microservices
 echo "Deploying traefik stack"
 docker stack deploy -c traefik/traefik-stack.yml traefik
+echo "Deplying shepherd stack"
 docker stack deploy -c shepherd/shepherd-stack.yml shepherd
+echo "Deploying microservices stack"
 docker stack deploy -c pimicroservices/pimicroservices-stack.yml microservices
---
+++ setupDockerMachine.sh
@@ -0,0 +1,4 @@
+docker-machine create --driver generic \
+  --generic-ip-address 10.0.0.96\
+  --generic-ssh-user=ubuntu \
+  pi4Cluster
